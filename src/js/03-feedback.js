3480e2d772bc03adfff093438dab74f6a17eeed3
Alona-Bulatkina <83859343+Alona-Bulatkina@users.noreply.github.com>
2021-10-31 15:15:32 +0200

    add 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,8 +1,31 @@
-import throttle from 'lodash.throttle'
+import throttle from 'lodash.throttle';
 
-
+const LOCALSSTORAGE_KEY = 'feedback-form-state'
 const filterForm = document.querySelector('.feedback-form');
+
+initForm();
+
 filterForm.addEventListener('submit', evt => {
     evt.preventDefault();
-    console.log(filterForm.elements);
-})
+    const formData = new FormData(filterForm);
+    formData.forEach((value, name) => console.log(value, name));
+});
+
+// filterForm.addEventListener('input', throttle(filterForm, 500));
+
+filterForm.addEventListener('change', evt => {
+let persistedFilters = localStorage.getItem(LOCALSSTORAGE_KEY);
+persistedFilters = persistedFilters ? JSON.parse(persistedFilters) : {};
+persistedFilters[evt.target.name] = evt.target.value;
+localStorage.setItem(LOCALSSTORAGE_KEY, JSON.stringify(persistedFilters));
+});
+
+function initForm () {
+    let persistedFilters = localStorage.getItem(LOCALSSTORAGE_KEY);
+    if (persistedFilters) {
+        persistedFilters = JSON.parse(persistedFilters);
+        Object.entries(persistedFilters).forEach(([name, value]) => {
+        filterForm.elements[name].value = value;
+    });
+    };
+};
