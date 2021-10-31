e8384433921cc4f12b79113ff81852a4e440c6d5
Alona-Bulatkina <83859343+Alona-Bulatkina@users.noreply.github.com>
2021-10-31 11:23:21 +0200

    add 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,15 +1,16 @@
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
-var options = {
+const options = {
     id: 59777392,
     width: 640,
     loop: true
 };
 
-var player = new Vimeo.Player('made-in-ny', options);
+const player = new Vimeo.Player('made-in-ny', options);
 
 player.setVolume(0);
 
-player.on('play', function() {
-    console.log('played the video!');
+player.on('eventName', function(data) {
+    
 });
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,8 @@
+import throttle from 'lodash.throttle'
 
+
+const filterForm = document.querySelector('.feedback-form');
+filterForm.addEventListener('submit', evt => {
+    evt.preventDefault();
+    console.log(filterForm.elements);
+})
